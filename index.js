961db1344d3f2804c5f98516d455d0c7dd97d227
= <=>
2023-07-04 20:17:57 -0400

    last 04-07

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.sendFile('index.html')
 })
 
+app.get('/cvfile',(req, res) => {
+  res.download(__dirname +'/www/assets/file/curriculum-vitae-SG.pdf')
+})
+
 app.listen(PORT, () => {
   console.log(`Listening Express on !!!! ${HOST}:${PORT}`);
 })
